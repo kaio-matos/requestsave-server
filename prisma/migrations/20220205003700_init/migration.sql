ca3f32ca1f429f76f8a766f8d3692006ad0b2844
Kaio <kaio_matos22@hotmail.com>
2022-02-05 10:34:49 -0300

    Prisma schema and migration

---
+++ prisma/migrations/20220205003700_init/migration.sql
@@ -0,0 +1,90 @@
+-- CreateTable
+CREATE TABLE `AccountTie` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `phoneNumber` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `AccountTie_phoneNumber_key`(`phoneNumber`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Account` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `first_name` VARCHAR(191) NOT NULL,
+    `last_name` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `confirmedEmail` BOOLEAN NOT NULL DEFAULT false,
+    `confirmedEmailToken` VARCHAR(191) NULL,
+    `confirmedEmailExpires` DATETIME(3) NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `passwordResetToken` VARCHAR(191) NULL,
+    `passwordResetExpires` DATETIME(3) NULL,
+    `role` VARCHAR(20) NOT NULL DEFAULT 'USER',
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `accountTie_id` INTEGER NOT NULL,
+
+    UNIQUE INDEX `Account_email_key`(`email`),
+    UNIQUE INDEX `Account_accountTie_id_key`(`accountTie_id`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Client` (
+    `account_id` INTEGER NOT NULL,
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `Client_name_key`(`name`),
+    UNIQUE INDEX `Client_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Product` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `basePrice` DOUBLE NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `account_id` INTEGER NOT NULL,
+
+    UNIQUE INDEX `Product_name_key`(`name`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Request` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(191) NOT NULL,
+    `status` VARCHAR(20) NOT NULL DEFAULT 'TODO',
+    `price` DOUBLE NOT NULL,
+    `paidOut` DOUBLE NOT NULL DEFAULT 0,
+    `balance` DOUBLE NOT NULL DEFAULT 0,
+    `expiresIn` DATETIME(3) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `product_id` INTEGER NOT NULL,
+    `client_id` INTEGER NOT NULL,
+
+    UNIQUE INDEX `Request_title_key`(`title`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Account` ADD CONSTRAINT `Account_accountTie_id_fkey` FOREIGN KEY (`accountTie_id`) REFERENCES `AccountTie`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Client` ADD CONSTRAINT `Client_account_id_fkey` FOREIGN KEY (`account_id`) REFERENCES `Account`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Product` ADD CONSTRAINT `Product_account_id_fkey` FOREIGN KEY (`account_id`) REFERENCES `Account`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Request` ADD CONSTRAINT `Request_client_id_fkey` FOREIGN KEY (`client_id`) REFERENCES `Client`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Request` ADD CONSTRAINT `Request_product_id_fkey` FOREIGN KEY (`product_id`) REFERENCES `Product`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;

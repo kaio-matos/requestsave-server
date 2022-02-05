d33e8d0f9c5bc5981374ef2ebfbcea95f4de3c66
Kaio <kaio_matos22@hotmail.com>
2022-02-05 10:37:10 -0300

    Adding Routes

---
+++ src/routes/accountRoutes.ts
@@ -0,0 +1,25 @@
+import { Router } from "express";
+
+import AccountController from "../controllers/AccountController";
+import auth from "../middlewares/auth";
+
+const routes = Router();
+
+routes.post("/register", AccountController.registerAndSendEmail);
+routes.post(
+  "/resendregisterconfirmation",
+  AccountController.resendRegisterConfirmation
+);
+routes.get("/confirmregistration", AccountController.confirmRegistration);
+
+routes.post("/login", AccountController.login);
+routes.post("/forgetpassword", AccountController.forgotSendEmail);
+routes.post("/forgetresetpassword", AccountController.forgotResetPass);
+
+routes.post("/resetpassword", auth, AccountController.resetPassword);
+routes.post("/checktoken", auth, AccountController.checkJWT);
+routes.post("/logout", auth, AccountController.logout);
+routes.patch("/edit", auth, AccountController.edit);
+routes.delete("/delete", auth, AccountController.delete);
+
+export default routes;

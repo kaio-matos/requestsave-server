5d597a255ae1a2d5975104ee540d54bb644eca67
Kaio <kaio_matos22@hotmail.com>
2022-02-07 21:29:14 -0300

    [routes] Adding routes

--- src/routes/accountRoutes.ts
+++ src/routes/accountRoutes.ts
@@ -6,20 +6,17 @@ import auth from "../middlewares/auth";
 const routes = Router();
 
 routes.post("/register", AccountController.registerAndSendEmail);
-routes.post(
-  "/resendregisterconfirmation",
-  AccountController.resendRegisterConfirmation
-);
+routes.post("/resendregisterconfirmation", AccountController.resendRegisterConfirmation);
 routes.get("/confirmregistration", AccountController.confirmRegistration);
 
-routes.post("/login", AccountController.login);
-routes.post("/forgetpassword", AccountController.forgotSendEmail);
-routes.post("/forgetresetpassword", AccountController.forgotResetPass);
+// routes.post("/login", AccountController.login);
+// routes.post("/forgetpassword", AccountController.forgotSendEmail);
+// routes.post("/forgetresetpassword", AccountController.forgotResetPass);
 
-routes.post("/resetpassword", auth, AccountController.resetPassword);
-routes.post("/checktoken", auth, AccountController.checkJWT);
-routes.post("/logout", auth, AccountController.logout);
-routes.patch("/edit", auth, AccountController.edit);
-routes.delete("/delete", auth, AccountController.delete);
+// routes.post("/resetpassword", auth, AccountController.resetPassword);
+// routes.post("/checktoken", auth, AccountController.checkJWT);
+// routes.post("/logout", auth, AccountController.logout);
+// routes.patch("/edit", auth, AccountController.edit);
+// routes.delete("/delete", auth, AccountController.delete);
 
 export default routes;
--- src/routes/panelRoutes.ts
+++ src/routes/panelRoutes.ts
@@ -1,38 +1,38 @@
 import { Router } from "express";
-import AdminController from "../controllers/Admin/AdminController";
+// import AdminController from "../controllers/Admin/AdminController";
 
-import UserController from "../controllers/UserController";
-import auth from "../middlewares/auth";
-import createModels from "../middlewares/createModels";
-import roleCheck from "../middlewares/roleCheck";
+// import UserController from "../controllers/UserController";
+// import auth from "../middlewares/auth";
+// import createModels from "../middlewares/createModels";
+// import roleCheck from "../middlewares/roleCheck";
 
 const routes = Router();
 
 // Checamos o usuário, e então criamos os seus respectivos modelos para alteração no DB
-routes.use(auth, createModels);
+// routes.use(auth, createModels);
 
-routes.post("/client", UserController.createClient);
-routes.patch("/client", UserController.editClient);
-routes.delete("/client", UserController.deleteClient);
-routes.get("/client", UserController.getClients);
+// routes.post("/client", UserController.createClient);
+// routes.patch("/client", UserController.editClient);
+// routes.delete("/client", UserController.deleteClient);
+// routes.get("/client", UserController.getClients);
 
-routes.post("/request", UserController.createRequest);
-routes.patch("/request", UserController.editRequest);
-routes.delete("/request", UserController.deleteRequest);
-routes.get("/request", UserController.getRequests);
+// routes.post("/request", UserController.createRequest);
+// routes.patch("/request", UserController.editRequest);
+// routes.delete("/request", UserController.deleteRequest);
+// routes.get("/request", UserController.getRequests);
 
-routes.post("/product", UserController.createProduct);
-routes.patch("/product", UserController.editProduct);
-routes.delete("/product", UserController.deleteProduct);
-routes.get("/product", UserController.getProducts);
+// routes.post("/product", UserController.createProduct);
+// routes.patch("/product", UserController.editProduct);
+// routes.delete("/product", UserController.deleteProduct);
+// routes.get("/product", UserController.getProducts);
 
-routes.patch("/users", roleCheck, AdminController.editAccountRole);
-routes.delete("/users", roleCheck, AdminController.deleteAccount);
-routes.get("/users", roleCheck, AdminController.getAccounts);
+// routes.patch("/users", roleCheck, AdminController.editAccountRole);
+// routes.delete("/users", roleCheck, AdminController.deleteAccount);
+// routes.get("/users", roleCheck, AdminController.getAccounts);
 
-routes.post("/phonenumber", roleCheck, AdminController.createPhoneNumber);
-routes.patch("/phonenumber", roleCheck, AdminController.editPhoneNumber);
-routes.delete("/phonenumber", roleCheck, AdminController.deletePhoneNumber);
-routes.get("/phonenumber", roleCheck, AdminController.getPhoneNumber);
+// routes.post("/phonenumber", roleCheck, AdminController.createPhoneNumber);
+// routes.patch("/phonenumber", roleCheck, AdminController.editPhoneNumber);
+// routes.delete("/phonenumber", roleCheck, AdminController.deletePhoneNumber);
+// routes.get("/phonenumber", roleCheck, AdminController.getPhoneNumber);
 
 export default routes;

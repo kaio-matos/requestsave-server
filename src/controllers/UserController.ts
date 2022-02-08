5ca128292977e59494f638b0fe80b08726ed3576
Kaio <kaio_matos22@hotmail.com>
2022-02-08 20:31:37 -0300

    [controllers/User] Adding editClient

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -2,6 +2,7 @@ import ClientController from "./ClientController";
 
 export class UserController {
   createClient = ClientController.create;
+  editClient = ClientController.edit;
 }
 
 export default new UserController();

464d57ff7d461f421d8d0e3cee967dbbf1ea6fde
Kaio <kaio_matos22@hotmail.com>
2022-02-08 21:05:50 -0300

    [controllers/User] Adding getClients

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -4,6 +4,7 @@ export class UserController {
   createClient = ClientController.create;
   editClient = ClientController.edit;
   deleteClient = ClientController.delete;
+  getClients = ClientController.get;
 }
 
 export default new UserController();

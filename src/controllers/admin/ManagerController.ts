31244d1c3c45963445782c22ec882d89afe4477b
Kaio <kaio_matos22@hotmail.com>
2022-02-09 19:32:50 -0300

    [controllers/admin/Manager] Adding editRole

--- src/controllers/admin/ManagerController.ts
+++ src/controllers/admin/ManagerController.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import { prisma } from "../../app";
+import ErrorDealer from "../../errors/ErrorDealer";
 import { ResMsg } from "../../utils/ResponseMessage";
+import { ManagerValidation } from "../../validations/ManagerValidate";
 
 class ManagerController {
   public async get(req: Request, res: Response): Promise<Response> {
@@ -20,6 +22,22 @@ class ManagerController {
     });
 
     return res.json(ResMsg("Usuários encontrados com sucesso", treatedAccounts));
+  }
+
+  public async editRole(req: Request, res: Response): Promise<Response> {
+    const { account_id, ...data } = req.body;
+
+    if (ManagerValidation.editRole(data).error) throw new ErrorDealer("Validation:Error");
+
+    const account = await prisma.account.findUnique({ where: { id: data.id } });
+    if (!account) throw new ErrorDealer("User:DontExist");
+
+    const a = await prisma.account.update({
+      where: { id: data.id },
+      data: { role: data.role },
+    });
+
+    return res.status(200).json(ResMsg("Cargo alterado com sucesso!", true));
   }
 }
 

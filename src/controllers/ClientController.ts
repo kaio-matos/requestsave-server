ecf191bfd5fefd563d060038b1918bd58934576c
Kaio <kaio_matos22@hotmail.com>
2022-02-09 11:15:15 -0300

    [controllers/Client] Fixing get status code

--- src/controllers/ClientController.ts
+++ src/controllers/ClientController.ts
@@ -61,7 +61,7 @@ class ClientController {
     });
 
     if (!CLIENTS.length) throw new ErrorDealer("Client:DontExist", "Nenhum cliente foi encontrado");
-    return res.status(201).json(ResMsg("Clientes encontrados com sucesso", CLIENTS));
+    return res.status(200).json(ResMsg("Clientes encontrados com sucesso", CLIENTS));
   }
 }
 export default new ClientController();

ce435dcafe96995b42f35a7239ec9e680567a6bd
Kaio <kaio_matos22@hotmail.com>
2022-02-09 12:06:20 -0300

    [controllers/User] Adding createProduct

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,10 +1,13 @@
 import ClientController from "./ClientController";
+import ProductController from "./ProductController";
 
 export class UserController {
   createClient = ClientController.create;
   editClient = ClientController.edit;
   deleteClient = ClientController.delete;
   getClients = ClientController.get;
+
+  createProduct = ProductController.create;
 }
 
 export default new UserController();

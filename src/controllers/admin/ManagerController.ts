017b5806326fba2b2546b937f717b4938881f812
Kaio <kaio_matos22@hotmail.com>
2022-02-25 17:13:51 -0300

    [controllers/admin/Manager] Adding pagination to get accounts

--- src/controllers/admin/ManagerController.ts
+++ src/controllers/admin/ManagerController.ts
@@ -6,7 +6,39 @@ import { ManagerValidation } from "../../validations/ManagerValidate";
 
 class ManagerController {
   public async get(req: Request, res: Response): Promise<Response> {
-    const accounts = await prisma.account.findMany();
+    const search = req.query.search;
+    const [firstName, lastName] = String(search).split(" ");
+
+    const pagination = {
+      page: parseInt(String(req.query.page)),
+      pageSize: parseInt(String(req.query.pageSize)),
+    };
+
+    let paginator = {};
+    if (typeof pagination.page === "number" && pagination.pageSize) {
+      paginator = {
+        skip: pagination.page * pagination.pageSize,
+        take: pagination.pageSize,
+      };
+    }
+
+    const filter = {
+      AND: [
+        {
+          firstName: { contains: firstName },
+          lastName: { contains: lastName },
+        },
+      ],
+    };
+
+    const accounts = await prisma.account.findMany({
+      where: filter,
+      include: { accountTie: { select: { phoneNumber: true } } },
+      ...paginator,
+    });
+
+    const quantity = await prisma.account.count({ where: filter });
+
     const treatedAccounts = accounts.map((acc) => {
       const {
         password,
@@ -21,7 +53,9 @@ class ManagerController {
       return rest;
     });
 
-    return res.json(ResMsg("Usuários encontrados com sucesso", treatedAccounts));
+    return res.json(
+      ResMsg("Usuários encontrados com sucesso", { table: treatedAccounts, quantity })
+    );
   }
 
   public async editRole(req: Request, res: Response): Promise<Response> {
@@ -41,7 +75,8 @@ class ManagerController {
   }
 
   public async delete(req: Request, res: Response): Promise<Response> {
-    const { account_id, id } = req.body;
+    const id = parseInt(req.params.id);
+
     if (ManagerValidation.id(id).error) throw new ErrorDealer("Validation:Error");
 
     const account = await prisma.account.findUnique({ where: { id } });
